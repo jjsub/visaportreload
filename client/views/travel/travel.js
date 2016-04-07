edaf6316af7beb57f41bf12bf891f5f910697377
jjsub <jsamesub@gmail.com>
2016-04-07 17:13:38 -0500

    Cleaning spaces and comments

--- client/views/travel/travel.js
+++ client/views/travel/travel.js
@@ -3,16 +3,9 @@
   'use strict';
 
   angular.module('visaPort')
-  .factory('Weather', ['$http', function($http){
-    function getConditions(country){
-      return $http.jsonp('http://api.wunderground.com/api/f0e6fdcbf9d5f727/conditions/q/' + country + '.json?callback=JSON_CALLBACK');
-    }
-    return {getConditions:getConditions};
-  }])
-  .controller('TravelCtrl', ['$scope', '$interval', 'Travel','$http', 'Weather', function($scope, $interval, Travel, $http, Weather){
+  .controller('TravelCtrl', ['$scope', '$interval', 'Travel','$http', function($scope, $interval, Travel, $http){
     $scope.country = '';
     $scope.hasSearched = false;
-
 
      var passportImages = [
       '../assets/image/passport/UniteState-passport.jpg',
@@ -37,7 +30,6 @@
       visaMap =['../assets/image/visamaps/visamapUSA.png'
     ];
 
-
    $scope.findCountry = function(){
       Travel.findCountry($scope.countryTo, $scope.countryFrom).then(function(response){
         $scope.countryOne = response.data;
@@ -50,9 +42,7 @@
      Travel.allCountry(collection).then(function(response){
        $scope.passImage(collection);
        $scope.allCountry = response.data.Country;
-        //console.log(response.data);
     });
-
    };
 
    $scope.passImage = function(country){
@@ -112,48 +102,5 @@
       $scope.flag  = passportImages[17];
     }
    };
-
-   /* $scope.findCountry = function(){
-      Weather.getConditions($scope.countryTo).then(function(response){
-        console.log(response);
-        $scope.temperature = response.data.current_observation.temp_f;
-        $scope.icon  = response.data.current_observation.icon_url;
-      });*/
-   //$scope.selectCollection('usavisarequire');
-
-
-
- // function geocode(address){
- //   var geocoder = new google.maps.Geocoder();
- //   geocoder.geocode({address:address}, function(results, status){
-    //  var loc  = results[0].formatted_address,
-    //      lat  = results[0].geometry.location.lat(),
-    //      lng  = results[0].geometry.location.lng();
-     // console.log(name, lat, lng);
-   // });
-   //
-/*
-  function getWeather(country){
-    debugger;
-    return $http.jsonp('http://api.wunderground.com/api/f0e6fdcbf9d5f727/conditions/q/' + country + '.json?callback=JSON_CALLBACK');
-  }
-
-
-
-                         var query = pos.coords.latitude +','+ pos.coords.longitude;
-                         weatherApi(query);
-
-                      function getConditions(query){
-                        weatherApi.conditions($scope.couuntryTo).then(function(response){
-                          $scope.temperature = response.data.current_observation.temp_f;
-                          $scope.icon = response.data.current_observation.icon_url;
-                        });
-
-                      }
-                      if($scope.country){getConditions($scope.country);}
-                    }];
-*/
-
   }]);
 })();
-
